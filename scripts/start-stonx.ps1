ea267b55c2434aa9f743ebb087bbc14f7b9e35f4
Melech Idan <idanarad@gmail.com>
2026-01-03 22:35:55 +0200

    p/e works

--- scripts/start-stonx.ps1
+++ scripts/start-stonx.ps1
@@ -1,10 +1,26 @@
-# Stonx startup script (Windows)
+# Stonx staelmSpvsERf5lPZ7ZPQEhxSoTweNldZLL
+
+
+$env:FMP_API_KEY="elmSpvsERf5lPZ7ZPQEhxSoTweNldZLL"
+$env:YAHOO_COOKIE="A1=d=AQABBDyDqWgCEG5u2362YtRQQtJnYFjnShcFEgABCAG3WmmNaV5DyyMAAiAAAAcIPIOpaFjnShc&S=AQAAAhoCZjmmteyEYYBhE4FLUEU;A3=d=AQABBDyDqWgCEG5u2362YtRQQtJnYFjnShcFEgABCAG3WmmNaV5DyyMAAiAAAAcIPIOpaFjnShc&S=AQAAAhoCZjmmteyEYYBhE4FLUEU;GUC=AQABCAFpWrdpjUIeXQSB&s=AQAAAI9HmR4x&g=aVlymg;A1S=d=AQABBDyDqWgCEG5u2362YtRQQtJnYFjnShcFEgABCAG3WmmNaV5DyyMAAiAAAAcIPIOpaFjnShc&S=AQAAAhoCZjmmteyEYYBhE4FLUEU"
 
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $root
 
 Write-Host "Starting Stonx Backend..." -ForegroundColor Cyan
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$root'; yarn workspace @stonx/server dev"
+#Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$root'; yarn workspace @stonx/server dev"
+
+$serverCmd = @"
+cd '$root'
+`$env:FMP_API_KEY='$($env:FMP_API_KEY)'
+`$env:YAHOO_COOKIE='$($env:YAHOO_COOKIE)'
+yarn workspace @stonx/server dev
+"@
+
+Start-Process powershell -ArgumentList "-NoExit", "-Command", $serverCmd
+
+
+
 
 Start-Sleep -Seconds 2
 
